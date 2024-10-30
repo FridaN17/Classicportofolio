4ecb5a2fa62311f454a57e9b0939267777761041
FridaN17 <nika.frida17@gmail.com>
2024-10-30 10:37:38 +0100

    initial commit

---
+++ Classicportofolio.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+$(this).toggleClass('fa-times');
+$('header').toggleClass('toggle');
+    });
+$(window).on('scroll load',function(){
+    $(this).removeClass('fa-times');
+    $('header').removeClass('toggle');
+});
+//smooth scrolling
+$('a[href*="#"]').on('click',function(e){
+
+e.preventDefault();
+$('html,body').animate({
+
+scrollTop : $($(this).attr('href')).offset().top,
+
+
+
+},
+500,
+'linear'
+);
+
+});
+
+
+});
+
+let cards = document.querySelectorAll(".card");
+let stackArea = document.querySelector(".stack-area");
+
+function rotateCards() {
+  let angle = 0;
+  cards.forEach((card) => {
+    if (card.classList.contains("active")) {
+      card.style.transform = `translate(-50%, -120vh) rotate(-48deg)`;
+    } else {
+      card.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+      angle = angle - 10;
+    }
+  });
+}
+
+rotateCards();
+
+window.addEventListener("scroll", () => {
+  let proportion =
+    stackArea.getBoundingClientRect().top / window.innerHeight;
+  if (proportion <= 0) {
+    let n = cards.length;
+    let index = Math.ceil((proportion * n) / 2);
+    index = Math.abs(index) - 1;
+    for (let i = 0; i < n; i++) {
+      if (i <= index) {
+        cards[i].classList.add("active");
+      } else {
+        cards[i].classList.remove("active");
+      }
+    }
+    rotateCards();
+  }
+});
+
+//Code for responsiveness
+
+function adjust() {
+  let windowWidth = window.innerWidth;
+  let left = document.querySelector(".left");
+  left.remove();
+  if (windowWidth < 800) {
+    stackArea.insertAdjacentElement("beforebegin", left);
+  } else {
+    stackArea.insertAdjacentElement("afterbegin", left);
+  }
+}
+adjust();
+
+//detect Resize
+
+window.addEventListener("resize", adjust);
+
+
+
+
+
+
+
+
+
